76685f8b7074cab54e5bd6a73e4c88edffcc1a8d
Olsi Mosha <164888477+olsi19@users.noreply.github.com>
2024-05-05 21:28:34 +0200

    commit

--- .ipynb_checkpoints/functions-checkpoint.py
+++ .ipynb_checkpoints/functions-checkpoint.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 
-
 def clean_dtypes(df):
 
     for col in df.columns:
@@ -14,30 +13,26 @@ def clean_dtypes(df):
                 df[col].fillna(df[col].mode()[0], inplace=True)
         except Exception as e:
             print(f"{e} during {col}")
-
-    df.drop(columns=["Unnamed: 0"], axis=1, inplace=True)
+    
     df.drop(columns=["Unnamed: 0"], axis=1, inplace=True)
     df["date_time"] = pd.to_datetime(df["date_time"])
     df["clnt_tenure_yr"] = df["clnt_tenure_yr"].astype(int)
     df["clnt_tenure_mnth"] = df["clnt_tenure_mnth"].astype(int)
     df["clnt_age"] = df["clnt_age"].apply(np.ceil).astype(int)
     df["gendr"] = df["gendr"].apply(lambda x: "U" if "X" in x else x)
-    df["num_accts"] = df["num_accts"].astype(int)
     df["calls_6_mnth"] = df["calls_6_mnth"].astype(int)
     df["logons_6_mnth"] = df["logons_6_mnth"].astype(int)
+    df['num_account'] = df['num_account'].replace(2.26, 2)
     return df
 
-
 def rename_columns(df):
-
-    df.rename(columns={"clnt_tenure_yr": "client_years",
-                            "clnt_tenure_mnth": "client_month",
-    df.rename(columns={"clnt_tenure_yr": "client_years",
-                            "clnt_tenure_mnth": "client_month",
-                            "clnt_age": "client_age",
-                            "gendr": "gender",
-                            "num_accts": "num_accounts",
-                            "bal": "balance",
-                            "calls_6_mnth": "calls_6_months",
-                            "logons_6_mnth": "logins_6"}, inplace=True)
+    
+    df.rename(columns={"clnt_tenure_yr":"client_years",
+                            "clnt_tenure_mnth":"client_month",
+                            "clnt_age":"client_age",
+                            "gendr":"gender",
+                            "num_accts":"num_accounts",
+                            "bal":"balance",
+                            "calls_6_mnth":"calls_6_months",
+                            "logons_6_mnth":"logins_6"},inplace= True)
     return df
